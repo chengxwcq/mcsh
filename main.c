6c6ef4a3a00f3aa1cfab5b74b825d254056f1eb5
cheng ma <cheng.ma1@yitu-inc.com>
2019-07-07 21:59:13 +0800

    add parse command function, need fix bug

--- main.c
+++ main.c
@@ -9,7 +9,8 @@
 
 void greeting();
 void validate_and_process(char*);
-void process(char*);
+void process(char**);
+char** parse_cmd(char* buf);
 //char* strip(char*, int);
 
 int main() {
@@ -47,19 +48,67 @@ void greeting() {
 
 // check if command is valid or not
 void validate_and_process(char* buf) {
-    char* cmd = strip(buf);
-    if (*cmd == '\0') {
-        return;
+    char** cmd = parse_cmd(buf);
+
+    int i;
+    for (i = 0; cmd[i] != NULL; i++) {
+        printf("%s\n", cmd[i]);
     }
+
     // currently don't support cmd with spaces in them, like ls -l
-    if (strcmp(cmd, "ls") == 0) {
+    if (strcmp(cmd[0], "ls") == 0) {
+    printf("===\n");
+    fflush(stdout);
+ 
         process(cmd);
     } else {
         printf("unknown cmd\n");
     }
 }
 
-void process(char* buf) {
+char** parse_cmd(char* buf) {
+    char* cmd = strip(buf);
+    int size = 1;
+    int index = 0;
+    char** res = malloc(size * sizeof(char*));
+    
+    int i;
+    int last_index = 0;
+    bool in_space = false;
+    for (i = 0; ; i++) {
+        if (index == size) {
+            size = size * 3 / 2;
+            res = realloc(res, size * sizeof(char *));
+        }
+        if (cmd[i] == ' ' || cmd[i] == '\n' || cmd[i] == '\t' || cmd[i] == '\0') {
+            if (in_space && cmd[i] != '\0') {
+                last_index++;
+                continue;
+            }
+            char *tmp = malloc((i - last_index + 1) * sizeof(char));
+            memcpy(tmp, &cmd[last_index], i-last_index);
+            tmp[i - last_index] = '\0';
+            res[index++] = tmp;
+            last_index = i + 1;
+            in_space = true;
+            if (cmd[i] == '\0') {
+                break;
+            }
+        } else {
+            in_space = false;
+        }
+    }
+
+    if (index == size) {
+        size += 1;
+        res = realloc(res, size);
+    }
+    res[index] = NULL;
+
+    return res;
+}
+
+void process(char** buf) {
     pid_t pid = fork();
     // TODO: mask sigint and other related signal....
     if (pid < 0) {
